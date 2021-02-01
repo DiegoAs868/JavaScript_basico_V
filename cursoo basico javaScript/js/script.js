e347c6aecb9e34ac50dc64d7eb369e0d7e4254af
DiegoAs868 <73847019+DiegoAs868@users.noreply.github.com>
2021-02-01 12:31:49 -0600

    Add files via upload

---
+++ cursoo basico javaScript/js/script.js
@@ -0,0 +1,23 @@
+//TIPOS DE DTOS EN JAVASCRIPT
+var saludo  "hola JavaScript"
+var edad = 6
+var unPoco = 1.5
+var siNo = true
+var seccion - "a"
+
+//MOSTRAR UN DATO CONSOLA
+console.log(saludo)
+
+
+//condicionales 
+
+var edad2 = prompt("Edad")
+
+if(edad2 >=18){
+    
+    alert("eres mayor de edad")
+    
+}else{
+    alert("eres menor de edad")
+}
+
